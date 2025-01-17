11f488eff4b06835e9e1be299798ce00cc7f6c7f
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2025-01-18 01:26:48 +0800

    [Fix] Routing again

--- AMSUFB/urls.py
+++ AMSUFB/urls.py
@@ -8,7 +8,7 @@ from webapp.views import superuser_login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-    path('fingerprint', include('fingerprint.urls')),
+    path('fingerprint/', include('fingerprint.urls')),
     path('login/', superuser_login, name='adminlogin'),
     path('user-login/', auth_views.LoginView.as_view(template_name='registration/user-login.html'), name='user-login'),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
